33d12bd0ea32f67699960390611ba11de13cf91f
kangnasharma46 <kangnasharma46@gmail.com>
2026-07-08 21:07:24 +0530

    count even module added

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ReactJs-InterviewExamples
-# ReactJs-InterviewExamples
--- src/app/counteven/page.tsx
+++ src/app/counteven/page.tsx
@@ -1,24 +1,38 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import React from "react";
 
 export default function CountEven() {
   const [inputValue, setInputValue] = useState("");
   const [count, setCount] = useState(0);
-  //   const countEvenNumber = (numberValues) => {
-  //     const numberArray = numberValues.split(",").map(Number);
-  //     setCount(
-  //       numberArray.filter((num) => Number.isInteger(num) && num % 2 === 0)
-  //         .length,
-  //     );
-  //   };
+  const [showCount, setshowCount] = useState(true);
+  const [error, setError] = useState(false);
+  const countEvenNumber = (numberValues) => {
+    const numberArray = numberValues
+      .split(",")
+      .map((item) => Number(item.trim()));
+    const isValid = numberArray.every((num) => !Number.isNaN(num));
+    if (isValid) {
+      setshowCount(true);
+      setCount(
+        numberArray.filter((num) => Number.isInteger(num) && num % 2 === 0)
+          .length,
+      );
+      setError(false);
+    } else {
+      setshowCount(false);
+      setCount(0);
+      setError(true);
+    }
+  };
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <div className="flex justify-center m-4 fixed top-2">
         <h1 className="text-4xl font-semibold">Count Even Numbers Component</h1>
       </div>
-      <div className="flex flex-col w-3/5 h-full bg-black items-center justify-center">
+      <div className="flex flex-col w-3/5 h-full bg-mauve-400 items-center justify-center">
         <h1>Enter number</h1>
         <input
           className="w-3/5 border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -29,10 +43,22 @@ export default function CountEven() {
         ></input>
         <button
           className="w-1/4 h-16 m-4 bg-orange-800 text-white border-2 border-white "
-          //   onClick={countEvenNumber}
+          onClick={() => countEvenNumber(inputValue)}
         >
           Count Even Numbers
         </button>
+        {error && (
+          <div className="flex justify-center m-4">
+            <h4 className="text-xl font-semibold text-red-800">
+              please enter only number with comma seprated
+            </h4>
+          </div>
+        )}
+        {showCount && (
+          <div className="flex justify-center m-4">
+            <h4 className="text-xl font-semibold">Total Count: {count}</h4>
+          </div>
+        )}
       </div>
     </div>
   );
